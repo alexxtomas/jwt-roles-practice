3bb5c3c838898cae5d7c388f89f3e0ae32207836
Alex Tomas <atomasllimos@gmail.com>
2022-11-24 11:25:05 +0100

    Modify auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,9 +13,7 @@ export const signUp = async (req, res) => {
 
   if (roles) {
     const foundRoles = await Role.find({ name: { $in: roles } })
-    if (foundRoles) {
-      newUser.roles = foundRoles.map((role) => role._id)
-    }
+    newUser.roles = foundRoles.map((role) => role._id)
   } else {
     const role = await Role.findOne({ name: 'user' })
     newUser.roles = [role._id]
